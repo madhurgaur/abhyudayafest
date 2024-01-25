040c5f6de2afaaa35ae5b70825abf1c251f57311
Madhur Gaur <madhurgaur9140@gmail.com>
2024-01-25 12:34:37 +0530

    navbar update

---
+++ my-app/src/components/HomePage/Navbar/Navbar.jsx
@@ -0,0 +1,82 @@
+import React from 'react'
+import { useState,useEffect } from 'react';
+import { RiMenu3Line } from "react-icons/ri";
+import { IoCloseOutline } from "react-icons/io5";
+import './navbar.css';
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import logo from'../../../Assets/abhyudatalogo.png';
+
+const Menu=()=>{
+  return (
+    <div>
+    <p className="nav-link"><a href='/'>Home</a></p>
+    <p className="nav-link"><a href='/events'>Events</a></p>
+    <p className="nav-link" ><a href='/registration'>Registration</a></p>
+    <p className="nav-link" ><a href='/team'>Team</a></p>
+    <p className="nav-link" ><a href='/contact'>Contact US</a></p>
+    </div>
+
+  );
+}
+
+const Navbar = () => {
+ 
+  useEffect(function () {
+    gsap.registerPlugin(ScrollTrigger);
+    const tl = gsap.timeline();
+    tl.from('#logo , .nav-link', {
+      y: -100,
+      duration: 0.5,
+      stagger:0.2,
+      delay:0.5
+      
+      
+    });
+
+    tl.from('.plane2', {
+      x: 20,
+      y: -25,
+      rotate: -20,
+      duration: 0.5,  
+    });
+  }, []);
+
+  const [toggleMenu, setToggleMenu] = useState(false); 
+
+
+  return (
+    <>
+    <div className='fest__navbar'>
+       <div className='fest__navbar-links'>
+         <div className='fest__navbar-links_logo'>
+           <img src={logo} alt='logo' id='logo' loading='lazy'/>
+           
+         </div>
+         <div className='fest__navbar-links_container'>
+          <Menu/>
+         </div>
+       </div>
+       <div className='fest__navbar-menu'>
+       <>
+       {toggleMenu ? (
+        <IoCloseOutline color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+      ) : (
+        <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />
+      ) }
+        { toggleMenu &&
+           (
+          <div className='fest__navbar-menu_container scale up centre'>
+           <div className='fest__navbar-menu_container-links'>
+             <Menu/>
+           </div>
+         </div>
+         )}
+         </>
+       </div>
+    </div>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ my-app/src/components/RegistrationPage/Registration.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import './registration.css';
+import Navbar from '../HomePage/Navbar/Navbar';
+import registering from '../../Assets/registration.png'
+<link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:opsz,wght@6..12,300&display=swap" rel="stylesheet"></link>
+
+const Registration = () => {
+  return (
+    <div className='registration'>
+    <div className='navbar'>
+        <Navbar/>
+      </div>
+      <div className='regContainer'>
+      <div className='regPng'>
+        <img src={registering} alt='reg' />
+      </div>
+      <div className='registrationContainer'>
+      <div className='regHeading'>
+       <h2>Register Now</h2>
+      </div>
+      <div className="form-container">
+    <div className="form-group">
+      <label for="name" className="form-label">Name:</label>
+      <input type="text" id="name" className="form-input" placeholder="Your Name"/>
+    </div>
+
+    <div className="form-group">
+      <label for="phone" className="form-label">Phone Number:</label>
+      <input type="tel" id="phone" className="form-input" placeholder="Your Phone Number"/>
+    </div>
+
+    <div className="form-group">
+      <label for="gender" className="form-label">Gender:</label>
+      <select id="gender" className="form-select">
+        <option value="male">Male</option>
+        <option value="female">Female</option>
+        <option value="other">Other</option>
+      </select>
+    </div>
+
+    <div className="form-group">
+      <label for="event" className="form-label">Event:</label>
+      <select id="event" className="form-select">
+        <option value="event1">Event 1</option>
+        <option value="event2">Event 2</option>
+        <option value="event3">Event 3</option>
+      </select>
+    </div>
+
+    <div className="form-group">
+      <label for="year" className="form-label">Year of Study:</label>
+      <input type="text" id="year" className="form-input" placeholder="Your Year of Study"/>
+    </div>
+
+    <div className="form-group">
+      <label for="college" className="form-label">College Name:</label>
+      <input type="text" id="college" className="form-input" placeholder="Your College Name"/>
+    </div>
+   
+    <div className='btn-div'>
+    <button className="form-button">Submit</button>
+    </div>
+  </div>
+  </div>
+  </div>
+    </div>
+  )
+}
+
+export default Registration;
