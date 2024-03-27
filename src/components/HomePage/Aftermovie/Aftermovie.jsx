20fa550cf9f442b6a06d5bcf5f9525e16c0ffc7c
Madhur Gaur <madhurgaur9140@gmail.com>
2024-03-27 11:08:03 +0530

    new aftermovie updated

--- src/components/HomePage/Aftermovie/Aftermovie.jsx
+++ src/components/HomePage/Aftermovie/Aftermovie.jsx
@@ -6,7 +6,7 @@ const Aftermovie = () => {
     <div className='aftermovie'>
     <h1>Abhyudaya AfterMovie</h1>
 <div className='video'>
-    <iframe width="780" height="440" src="https://www.youtube.com/embed/EyKG7f98nBU?si=S439_HDQOpEydE96" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" id='vid' allowfullscreen></iframe>
+    <iframe width="780" height="440" src="https://www.youtube.com/embed/kII6_AuglfY?si=AkGmquBY5gbjlbV2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" id='vid' allowfullscreen></iframe>
     </div>
     </div>
   )
